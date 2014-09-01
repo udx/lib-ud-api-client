b47cb7ec5799613f0c9e1760bfbf3f4ce42fec26
--add <maxim.peshkov@usabilitydynamics.com>
2014-09-01 17:05:46 +0300

    moved classes from API client example

---
+++ lib/Manager.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 
+ *
+ * @namespace UsabilityDynamics
+ *
+ */
+namespace UsabilityDynamics\UD_API {
+
+  if( !class_exists( 'UsabilityDynamics\UD_API\Manager' ) ) {
+
+    /**
+     * 
+     * @author: peshkov@UD
+     */
+    class Manager {
+
+      
+    }
+  
+  }
+  
+}
